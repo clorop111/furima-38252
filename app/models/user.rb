1ea84bc6b86543719b6962a1dfafce083e4afd2f
clorop111 <clorop111@icloud.com>
2022-11-09 18:39:17 +0900

    ユーザー機能修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
   validates :password, format: { with: VALID_PASSWORD_REGEX}
 
   validates :name, presence: true
-  validates :first_name_kanji, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :last_name_kanji, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name_kanji, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
+  validates :last_name_kanji, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/}
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :date_of_birth, presence: true
 
-  has_many :items
-  has_many :records
+  # has_many :items
+  # has_many :records
 
 end
