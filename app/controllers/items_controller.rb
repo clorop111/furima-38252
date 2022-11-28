25947cfcb93a57eb26dd52dc720f4bdf2950260c
clorop111 <clorop111@icloud.com>
2022-11-28 18:58:12 +0900

    商品一覧機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show]
+  # before_action :set_item, only: [:edit, :show]
   
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,27 +19,25 @@ class ItemsController < ApplicationController
     end
   end
 
-  def destroy
-  end
 
-  def edit
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to action: :index
-    end
-  end
+  # def edit
+  #   unless user_signed_in? && current_user.id == @item.user_id
+  #     redirect_to action: :index
+  #   end
+  # end
 
-  def show
+  # def show
     
-  end
+  # end
 
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-        redirect_to item_path(@item)
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   @item = Item.find(params[:id])
+  #   if @item.update(item_params)
+  #       redirect_to item_path(@item)
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   private
 
