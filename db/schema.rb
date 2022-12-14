437d07f994c2ef2f9e9168b71efed633cc803125
clorop111 <clorop111@icloud.com>
2022-12-14 21:18:16 +0900

    マイグレーションファイル変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_09_072623) do
+ActiveRecord::Schema.define(version: 2022_12_09_065242) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -55,10 +55,10 @@ ActiveRecord::Schema.define(version: 2022_12_09_072623) do
     t.string "address", null: false
     t.string "building_name"
     t.string "phone_number", null: false
-    t.bigint "item_id", null: false
+    t.bigint "record_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["record_id"], name: "index_orders_on_record_id"
   end
 
   create_table "records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -90,7 +90,7 @@ ActiveRecord::Schema.define(version: 2022_12_09_072623) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
-  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "records"
   add_foreign_key "records", "items"
   add_foreign_key "records", "users"
 end
