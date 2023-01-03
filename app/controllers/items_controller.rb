6abff58a5ef7ac0a05819a96a8aebef1bfbf713b
clorop111 <clorop111@icloud.com>
2023-01-03 15:55:38 +0900

    複数画像選択機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,7 +50,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :title, :explanation, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_required_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :explanation, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_required_id, :price, {images: []}).merge(user_id: current_user.id)
   end
 
   def set_item
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,13 +6,14 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days_required
 
-  has_one_attached :image
+  has_many_attached :images
 
 
   belongs_to :user
   has_one :record
 
-  validates :image, presence: true
+  validates :images, presence: true
+  validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
   validates :title, presence: true
   validates :explanation, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
